9402866298edb14418d58a542e73a9ba51907821
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-06-02 15:28:23 -0300

    Initial commit

---
+++ Assets/Scripts/Components/RobotDimension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct RobotDimension : IComponentData
+{
+    public float BodyRadius;
+    public float WheelRadius;
+    public float Height;
+}
---
+++ Assets/Scripts/Systems/RobotMovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using Unity.Transforms;
+using UnityEngine;
+
+public class RobotMovementSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Dependency = Entities.WithBurst().ForEach((ref PhysicsVelocity velocity, in PhysicsMass mass, in Rotation rotation, in WheelSpeed wheelSpeed, in RobotDimension dimension) => {
+            var linear = (wheelSpeed.Left + wheelSpeed.Right) * dimension.WheelRadius / 2f;
+            var angular = dimension.WheelRadius / dimension.BodyRadius * (wheelSpeed.Left - wheelSpeed.Right);
+
+            velocity.Linear = math.rotate(rotation.Value, new float3(0f, 0f, linear));
+            velocity.SetAngularVelocity(mass, rotation, new float3(0f, angular, 0f));
+        }).ScheduleParallel(Dependency);
+    }
+}
