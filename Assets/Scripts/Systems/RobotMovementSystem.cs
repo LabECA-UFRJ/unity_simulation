5dc5d00965ba49752829117a9f5cac4a3ba5fdeb
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-06-02 16:38:58 -0300

    Add max speed component to robot dynamics
    Wheel speed component now uses [-1, 1] range of max speed.

--- Assets/Scripts/Systems/RobotMovementSystem.cs
+++ Assets/Scripts/Systems/RobotMovementSystem.cs
@@ -10,9 +10,12 @@ public class RobotMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
-        Dependency = Entities.WithBurst().ForEach((ref PhysicsVelocity velocity, in PhysicsMass mass, in Rotation rotation, in WheelSpeed wheelSpeed, in RobotDimension dimension) => {
-            var linear = (wheelSpeed.Left + wheelSpeed.Right) * dimension.WheelRadius / 2f;
-            var angular = dimension.WheelRadius / dimension.BodyRadius * (wheelSpeed.Left - wheelSpeed.Right);
+        Dependency = Entities.WithBurst().ForEach((ref PhysicsVelocity velocity, in PhysicsMass mass, in Rotation rotation, in WheelSpeed wheelSpeed, in RobotDimension dimension, in RobotDynamics dynamics) => {
+            var leftWheelSpeed = math.clamp(wheelSpeed.Left, -1f, 1f) * dynamics.MaxSpeed;
+            var rightWheelSpeed = math.clamp(wheelSpeed.Right, -1f, 1f) * dynamics.MaxSpeed;
+
+            var linear = (leftWheelSpeed + rightWheelSpeed) * dimension.WheelRadius / 2f;
+            var angular = dimension.WheelRadius / dimension.BodyRadius * (leftWheelSpeed - rightWheelSpeed);
 
             velocity.Linear = math.rotate(rotation.Value, new float3(0f, 0f, linear));
             velocity.SetAngularVelocity(mass, rotation, new float3(0f, angular, 0f));
