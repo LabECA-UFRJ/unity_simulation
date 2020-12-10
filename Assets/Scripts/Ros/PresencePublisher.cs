986b5e6f83d7a59f85b8f848d48b08d5c6c53148
Milena <milenaferreira294@hotmail.com>
2020-12-09 22:55:59 -0300

    Add ros publisher to advertise presence

---
+++ Assets/Scripts/Ros/PresencePublisher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Ros
+{
+    public class PresencePublisher : MonoBehaviour
+    {
+        public ROSConnection ros;
+        public string topicName = "presence";
+        
+        void Start()
+        {
+            ros.Send(topicName, new RosMessageTypes.Std.Int32(0));
+        }
+    }
+}
