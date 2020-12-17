036d58dd22547ef0d25d199c9044ce3a7186d9da
Milena <milenaferreira294@hotmail.com>
2020-12-16 22:33:09 -0300

    Connect subscriber to spawn robots
    Class responsible for subscribing and receiving the information needed to spawn a new nobot

---
+++ Assets/Scripts/Ros/RobotSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using System;
+
+namespace Ros{
+    public class RobotSpawner : MonoBehaviour
+    {
+        public ROSConnection ros;
+        public string topicName = "robot_def";
+        public GameObject robotPrefab;
+
+        void Start()
+        {
+            ros.Subscribe<RosMessageTypes.Simulation.Robot>(topicName, SpawnRobot);
+        }
+
+        void SpawnRobot(RosMessageTypes.Simulation.Robot robotMessage)
+        {
+            Vector3 position = new Vector3((float)robotMessage.position.x, (float)robotMessage.position.y, (float)robotMessage.position.z);
+
+            GameObject robot = Instantiate(robotPrefab, position, Quaternion.AngleAxis(robotMessage.rotation, Vector3.up));
+            robot.GetComponent<RobotIdAuthoring>().id = robotMessage.robot_id;
+        }
+    }
+}
