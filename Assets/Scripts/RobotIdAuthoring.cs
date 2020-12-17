3e5c7daa8af6fe0335e418f7ee8a9c9c3788430e
Milena <milenaferreira294@hotmail.com>
2020-12-16 22:31:36 -0300

    Create robot id authoring class
    Class used to acess and modify the robot id after instantiation

---
+++ Assets/Scripts/RobotIdAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class RobotIdAuthoring: MonoBehaviour, IConvertGameObjectToEntity 
+{
+  public int id;
+  
+  public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+  {
+    var robotId = new RobotId { Value = id }; // A component data that has only one float called Value
+    dstManager.AddComponentData(entity, robotId);
+  }
+}
