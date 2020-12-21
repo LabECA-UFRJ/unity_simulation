a6897e9ec7ab1925b7c56e8e8d4ddbb0f5a2b81d
Milena <milenaferreira294@hotmail.com>
2020-12-21 12:00:31 -0300

    Create system to update robot's wheel speed

---
+++ Assets/Scripts/Systems/RobotWheelSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using RosMessageTypes.Simulation;
+
+public class RobotWheelSystem : SystemBase
+{
+    private ROSConnection ros;
+    private string topicName = "robot_cmd_vel";
+
+    protected override void OnCreate()
+    {
+        ros = Object.FindObjectOfType<ROSConnection>();
+        ros.Subscribe<RobotWheelSpeed>(topicName, UpdateWheelSpeed);
+    }
+
+    private void UpdateWheelSpeed(RobotWheelSpeed message)
+    {
+        Entities.WithoutBurst().ForEach((ref WheelSpeed wheelSpeed, in RobotId robotId) => {
+            if (message.id == robotId.Value){
+                wheelSpeed.Left = message.left;
+                wheelSpeed.Right = message.right;
+            }
+        }).Run();
+    }
+
+    protected override void OnUpdate()
+    {
+    }
+}
