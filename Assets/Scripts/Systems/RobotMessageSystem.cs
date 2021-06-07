a08c650a1a99157bd553036998754fd8c06d4f0b
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2021-06-07 15:01:44 -0300

    fix: send correct time data in header stamp

--- Assets/Scripts/Systems/RobotMessageSystem.cs
+++ Assets/Scripts/Systems/RobotMessageSystem.cs
@@ -3,6 +3,7 @@ using Unity.Entities;
 using Unity.Transforms;
 
 using ROSGeometry;
+using System.Diagnostics;
 
 public class RobotMessageSystem : SystemBase
 {
@@ -13,9 +14,14 @@ public class RobotMessageSystem : SystemBase
     private float _nextUpdate = 0f;
     private const float _updateTime = 1 / 120f;
 
+    private Stopwatch _stopWatch;
+
     protected override void OnCreate()
     {
         ros = Object.FindObjectOfType<ROSConnection>();
+
+        _stopWatch = new Stopwatch();
+        _stopWatch.Start();
     }
 
     protected override void OnUpdate()
@@ -53,11 +59,28 @@ public class RobotMessageSystem : SystemBase
         }).Run();
 
         var msgHeader = new RosMessageTypes.Std.Header();
-        msgHeader.stamp = new RosMessageTypes.Std.Time((uint)System.DateTime.Now.Second, (uint)System.DateTime.Now.Millisecond / 1000);
+        msgHeader.stamp = new RosMessageTypes.Std.Time(_stopWatch.Seconds(), _stopWatch.Nanoseconds());
         msgHeader.frame_id = referenceFrame;
 
         var message = new RosMessageTypes.Simulation.PoseRobotArray(msgHeader, robotsInformation);
 
         ros.Send(topicName, message);
+    }
+}
+
+public static class StopwatchExtensions
+{
+    public static uint Seconds(this Stopwatch stopwatch)
+    {
+        long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+        return (uint)(elapsedMilliseconds / 1000);
+    }
+
+    public static uint Nanoseconds(this Stopwatch stopwatch)
+    {
+        long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+        long millisecondsAboveSecond = elapsedMilliseconds - (elapsedMilliseconds / 1000);
+        return (uint)(millisecondsAboveSecond * 1_000_000);
     }
 }
